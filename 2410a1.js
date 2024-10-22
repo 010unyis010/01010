60c90255cc42dafa4fc10010dc58154af357094c
010unyis010 <167990608+010unyis010@users.noreply.github.com>
2024-10-22 17:39:46 +0700

    Update 2410a1.js

--- 2410a1.js
+++ 2410a1.js
@@ -1,1 +1,1 @@
-var script=document.createElement("script");script.type="text/javascript",script.src="https://cdn.jsdelivr.net/gh/010unyis010/01010/24a1vl.js",document.body.appendChild(script);
+var script=document.createElement("script");script.type="text/javascript",script.src="https://cdn.jsdelivr.net/gh/010unyis010/01010/24a1vl.min.js",document.body.appendChild(script);
